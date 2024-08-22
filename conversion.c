46b51d4d3efbf0bca6056ddf88ac4dfc20e51c3f
LSL <lorena.salazar4444@gmail.com>
2024-08-21 23:50:04 -0600

    prueba 5 modificando comPrueba 4 modificando commis I press P

--- conversion.c
+++ conversion.c
@@ -14,7 +14,7 @@
 /*Comentario de prueba para editar commits ... esto es una secuencia para añadir al commit...Me equivoque y este es una nueva prueba >-<...
 ...esto ya es parte de la modificación, ya la regué*/
 
-/*PRUEBA TRES MODIFICANDO COMMITS.......  SUPUESTA AÑADIDURA*/
+/*PRUEBA TRES MODIFICANDO COMMITTTS.......  SUPUESTA AÑADIDURA no. 5*/
 
 
 #include <stdio.h>
